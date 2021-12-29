43439d7e1b4ecc6691a361b9ccf3b38597fad2da
andriati-alex <andriati.alex@gmail.com>
2021-12-29 20:06:06 -0300

    Some customization in the colorscheme

--- nvim_lua/lua/colorscheme_config.lua
+++ nvim_lua/lua/colorscheme_config.lua
@@ -4,6 +4,34 @@
 -- sonokai
 -- kanagawa
 -- darkplus
+
+--vim.cmd [[let g:sonokai_style = 'shusia' ]]
+--vim.cmd [[let g:sonokai_transparent_background = 1 ]]
+--vim.cmd [[let g:sonokai_enable_italic = 1 ]]
+--vim.cmd [[let g:sonokai_diagnostic_virtual_text = 'colored' ]]
+--vim.cmd [[let g:sonokai_diagnostic_text_highlight = 1 ]]
+--vim.cmd [[let g:sonokai_current_word = 'underline' ]]
+
+local overrides_fields = {
+    TSOperator = { style = 'bold'},
+}
+
+-- Default options:
+require('kanagawa').setup({
+    undercurl = true,           -- enable undercurls
+    commentStyle = "italic",
+    functionStyle = "NONE",
+    keywordStyle = "italic",
+    statementStyle = "bold",
+    typeStyle = "NONE",
+    variablebuiltinStyle = "italic",
+    specialReturn = true,       -- special highlight for the return keyword
+    specialException = true,    -- special highlight for exception handling keywords
+    transparent = true,         -- do not set background color
+    colors = {},
+    overrides = overrides_fields,
+})
+
 local colorscheme = "kanagawa"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
