8b1b3c03ca14f52e2975cda12ae712a09f6a840b
andriati-alex <andriati.alex@gmail.com>
2021-12-28 22:32:31 -0300

    Few tweaks in nvim_lua

--- nvim_lua/lua/autocompletion_config.lua
+++ nvim_lua/lua/autocompletion_config.lua
@@ -46,7 +46,7 @@ local kind_icons = {
   Struct = "",
   Event = "",
   Operator = "",
-  TypeParameter = ""
+  TypeParameter = ""
 }
 
 cmp.setup {
--- nvim_lua/lua/lualine_config.lua
+++ nvim_lua/lua/lualine_config.lua
@@ -45,7 +45,7 @@ require'lualine'.setup {
   },
   tabline = {
       lualine_a = {'buffers'},
-      lualine_b = {'branch'},
+      lualine_b = {},
       lualine_c = {},
       lualine_x = {},
       lualine_y = {},
